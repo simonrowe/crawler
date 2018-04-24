244a24c1aa77e60ca70fc252cf55b47bea989fd3
Simon Rowe <simon.rowe@umusic.com>
2018-04-24 07:16:11 +0100

    add tests and implementation for image on single page.

--- src/main/java/com/simonjamesrowe/crawler/service/CrawlerService.java
+++ src/main/java/com/simonjamesrowe/crawler/service/CrawlerService.java
@@ -1,19 +1,23 @@
 package com.simonjamesrowe.crawler.service;
 
+import com.simonjamesrowe.crawler.model.Image;
 import com.simonjamesrowe.crawler.model.Page;
 import com.simonjamesrowe.crawler.model.WebElement;
 import com.simonjamesrowe.crawler.model.dao.CrawlerDao;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import java.util.ArrayList;
 
 public class CrawlerService {
 
   private static final String XPATH_TITLE_SELECTOR = "//head/title";
+  private static final String XPATH_IMAGE_SELECTOR = "//img";
 
   private XPath xPath;
 
@@ -30,6 +34,44 @@ public class CrawlerService {
     Page page = new Page();
     page.setUri(uri);
     page.setTitle(title.getTextContent());
+    NodeList nodeList =
+        (NodeList)
+            xPath.compile(XPATH_IMAGE_SELECTOR).evaluate(xmlDocument, XPathConstants.NODESET);
+
+    if (nodeList.getLength() > 0) {
+      page.setContents(new ArrayList<>());
+    }
+
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Node node = nodeList.item(i);
+      Image image = new Image();
+      image.setUri(getAbsoluteUri(uri, node.getAttributes().getNamedItem("src").getNodeValue()));
+      page.getContents().add(image);
+    }
+
     return page;
+  }
+
+  protected String getAbsoluteUri(String uri, String src) {
+    if (src.startsWith("http")) {
+      return src;
+    }
+
+    String relativeSrc = src.startsWith("/") ? src.substring(1) : src;
+    if (src.startsWith("/")) {
+      int domainPathSeperator = uri.indexOf("/", 7);
+      if (domainPathSeperator == -1) {
+        return uri + "/" + relativeSrc;
+      } else {
+        return uri.substring(0, domainPathSeperator + 1) + relativeSrc;
+      }
+    } else {
+      int lastPath = uri.lastIndexOf("/");
+      if (lastPath == 6) {
+        return uri + "/" + relativeSrc;
+      } else {
+        return uri.substring(0, lastPath + 1) + relativeSrc;
+      }
+    }
   }
 }
--- src/test/java/com/simonjamesrowe/crawler/service/CrawlerServiceTest.java
+++ src/test/java/com/simonjamesrowe/crawler/service/CrawlerServiceTest.java
@@ -1,5 +1,6 @@
 package com.simonjamesrowe.crawler.service;
 
+import com.simonjamesrowe.crawler.model.Image;
 import com.simonjamesrowe.crawler.model.Page;
 import com.simonjamesrowe.crawler.model.WebElement;
 import com.simonjamesrowe.crawler.model.dao.CrawlerDao;
@@ -37,6 +38,52 @@ public class CrawlerServiceTest {
     Page page = (Page) result;
     assertEquals("http://www.example.com", result.getUri());
     assertEquals("Example.com - A great example of simplicity", page.getTitle());
+  }
+
+  @Test
+  public void testCrawlWithSinglePageTitleAndImages() throws Exception {
+    String uri = "http://www.example.com";
+    given(crawlerDao.webContent(eq(uri))).willReturn(pageContent("home.html"));
+    WebElement result = crawlerService.crawl(uri);
+    assertNotNull(result);
+    assertTrue(result instanceof Page);
+    Page page = (Page) result;
+    assertEquals("http://www.example.com", result.getUri());
+    assertEquals("Example.com - A great example of simplicity", page.getTitle());
+    assertNotNull(page.getContents());
+    assertTrue(page.getContents().size() >= 1);
+    assertEquals(
+        "http://www.example.com/us.jpg",
+        page.getContents().stream().filter(pc -> pc instanceof Image).findFirst().get().getUri());
+  }
+
+  @Test
+  public void testGetAbsoluteUrl() {
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com", "img.jpg"));
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/", "img.jpg"));
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/", "/img.jpg"));
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/something", "/img.jpg"));
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/something/", "/img.jpg"));
+    assertEquals(
+        "http://www.example.com/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/something", "img.jpg"));
+    assertEquals(
+        "http://www.example.com/something/img.jpg",
+        crawlerService.getAbsoluteUri("http://www.example.com/something/", "img.jpg"));
+    assertEquals(
+        "http://www.other.com/img.jpg",
+        crawlerService.getAbsoluteUri(
+            "http://www.example.com/something/", "http://www.other.com/img.jpg"));
   }
 
   private Document pageContent(String file) throws Exception {
